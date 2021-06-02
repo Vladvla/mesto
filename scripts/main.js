fbeed9544fd8f9b1a488309675f5e7edc402af53
Vladislav Shchukin <v.schukin.99@yandex.ru>
2021-06-03 02:07:51 +0700

    feat: second popup and opacityPopup

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,42 @@
-// элементы открытия и закрытия попапа
+// Массив карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+// элементы открытия и закрытия попап'ов
 let openPopup = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closePopup = popup.querySelector('.popup__button-close')
+let closePopup = popup.querySelectorAll('.popup__button-close');
+let popupAddItem = document.querySelector('.popup-additem');
+let openPopupAddItem = document.querySelector('.profile__add-button');
 // Находим форму в DOM
 let formElement = document.querySelector('#popup__editForm');
 // Находим поля формы в DOM
 let nameInput = document.querySelector('#name-input');
 let jobInput = document.querySelector('#role-input');
+let imgInput = document.querySelector('#img-input');
 // Элементы куда должны быть вставлены значения полей формы
 let nameProfile = document.querySelector('.profile__name');
 let nameRole = document.querySelector('.profile__role');
@@ -17,18 +47,29 @@ function openPopupEvent(event) {
   jobInput.value = nameRole.textContent;
 }
 
+function openPopupAddItemEvent(event) {
+  popupAddItem.classList.add('popup_opened');
+};
+
+function renderClosePopupEvent() {
+  closePopup.forEach(closePopupEvent);
+}
+
 function closePopupEvent(event) {
   popup.classList.remove('popup_opened');
 }
 
 openPopup.addEventListener('click', openPopupEvent);
+openPopupAddItem.addEventListener('click', openPopupAddItemEvent);
 closePopup.addEventListener('click', closePopupEvent);
 
-popup.addEventListener('click', function(event) {
+popup.addEventListener('mousedown', function(event) {
   if (event.target === event.currentTarget) {
     closePopupEvent()
   }
 });
+
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
